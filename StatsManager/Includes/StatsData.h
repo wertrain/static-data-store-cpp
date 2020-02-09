e834e25511ec560cb796e5d31bd00f8237f180b5
wertrain <wertrain@gmail.com>
2020-02-10 00:27:30 +0900

    WIP

--- StatsManager/Includes/StatsData.h
+++ StatsManager/Includes/StatsData.h
@@ -6,9 +6,12 @@
 class StatsData
 {
 public:
+    static const int MaxDataNum = 1024;
+
+public:
     StatsData();
     void Clear();
 
 private:
-    std::array<StatsValue, 1024> m_values;
+    std::array<StatsValue, MaxDataNum> m_values;
 };
